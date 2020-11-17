5e2138749eadbfe8b744af039e24016c193b6472
takayama <takayama@foxmail.com>
2020-11-17 22:53:53 +0900

    update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,15 +1,13 @@
 "use strict";
-
-// 为了不在更新时出现冲突
-// 今后使用此config.sample.js文件作为样板文件
-// 需自行更名为config.js
+// 将此文件更名为config.js才能生效哦
+// 如果需要更详细的日志，可以在general中添加配置项 log_level: "trace",
+// 其他可用的日志等级："warn", "error", "off"
 
 module.exports = {
 
     //通用配置
     general: {
         platform:           2,      //1手机 2平板 3手表(部分事件不支持)
-        kickoff:            false,  //被挤下线是否在3秒后反挤对方
         ignore_self:        true,   //群聊和私聊是否无视自己的发言
         resend:             true,   //被风控的消息是否尝试以另一种方式再次发送(目前不清楚会有什么弊端，但是确实有效)
         debug:              false,  //开启debug
