370d9d5fe0a41fd352b166331d7bc836a9d3f8f2
takayama <takayama@foxmail.com>
2021-09-07 22:46:48 +0900

    #48

--- config.sample.js
+++ config.sample.js
@@ -28,7 +28,7 @@ module.exports = {
         ws_reverse_url: [ //反向ws地址，可以添加多个url
             // "ws://your.address.com:8080",
         ],
-        ws_reverse_reconnect_interval: 3000, //反向ws断线重连间隔(毫秒)，设为0直接不重连
+        ws_reverse_reconnect_interval: 3000, //反向ws断线重连间隔(毫秒)，设为负数直接不重连
         ws_reverse_reconnect_on_code_1000: true, //反向ws是否在关闭状态码为1000的时候重连
     },
 
